ef05f2aac444139d22b713933414b6c061d6f1e9
Huauauaa <Huauauaa@users.noreply.github.com>
2021-07-07 05:25:29 +0000

    deploy: 55a09653f1fb1b9095226b73a93dd6994bf65a46

---
+++ component---src-pages-about-js-d7b14681da9b3b8accd0.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"3XHS":function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),o=a.n(n),l=a("Wbzz"),c=a("Bl7J"),r=a("vrFN");t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"about page"}),o.a.createElement("h1",null,"About"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-about-js-d7b14681da9b3b8accd0.js.map
